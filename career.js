0d4c1629a81bca0cf3b36fc82e94dbef922e7e01
Josiahhicks555 <70174168+Josiahhicks555@users.noreply.github.com>
2020-12-09 13:18:25 -0500

    Create career.js

---
+++ career.js
@@ -0,0 +1,35 @@
+// Enter your code below to edit index.html
+
+yourName = "Josiah Hicks"
+document.getElementById("yourName").innerHTML = yourName;
+
+myBio = "I Like to run and play videogames"
+document.getElementById("myBio").innerHTML = myBio;
+
+programChoice = "Youngstown state and to take a program with programming and code"
+document.getElementById("programChoice").innerHTML = programChoice;
+
+collegeReason = "I picked this college because it has a good programming corse and the cost of going is not to high"
+document.getElementById("collegeReason").innerHTML = collegeReason;
+
+careerChoice = "I want to get a career designing games."
+document.getElementById("careerChoice").innerHTML = careerChoice;
+
+companyChoice = "I want to join a company like rockstar or mojang."
+document.getElementById("companyChoice").innerHTML = companyChoice;
+
+careerReason = "I chose this company because they make great games that i think i could help inprove and get more sales."
+document.getElementById("careerReason").innerHTML = careerReason;
+
+answerOne = "is an application programming interface for HTML documents. It defines the logical structure of documents and the way a document is accessed and manipulated"
+document.getElementById("answerOne").innerHTML = answerOne;
+
+answerTwo = "Writing into an HTML element, using innerHTML . Writing into the HTML output using document.write() . Writing into an alert box, using window.alert() . Writing into the browser console, using console.log()"
+document.getElementById("answerTwo").innerHTML = answerTwo;
+
+answerThree = "With the object model, JavaScript gets all the power it needs to create dynamic HTML, JavaScript can change all the HTML elements, HTML attributes, CSS styles"
+document.getElementById("answerThree").innerHTML = answerThree;
+
+
+
+
